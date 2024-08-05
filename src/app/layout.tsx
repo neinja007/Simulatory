434c65fb6af64f82492c9799055e6bb25cf245aa
neinja007 <anton.siligan@gmail.com>
2024-08-05 15:19:06 +0200

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Rubik } from 'next/font/google';
+import "./globals.css";
+
+const rubik = Rubik({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: "Simulatory",
+  description: "Mathematical Simulations for Creative Minds designed to break the boundaries of traditional simulation.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={rubik.className}>{children}</body>
+    </html>
+  );
+}
