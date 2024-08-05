a5ed1fd15959556c90bbc715abe58a605a9646e1
neinja007 <anton.siligan@gmail.com>
2024-08-05 18:01:24 +0200

    chore: Add new layout component for simulations page

---
+++ src/app/(simulations)/layout.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import Header from '@/components/ui/header';
+import { routes } from '@/data/routes';
+import { usePathname } from 'next/navigation';
+
+type LayoutProps = { children: React.ReactNode };
+
+const Layout = ({ children }: LayoutProps) => {
+  const pathname = usePathname();
+  const route = routes.find((route) => route.href === pathname);
+
+  if (!route) {
+    throw new Error(`Route not found for pathname: ${pathname}`);
+  }
+
+  return (
+    <div className='mx-auto max-w-[900px]'>
+      <Header title={route.name} />
+      <span className='mb-8 text-slate-500 dark:text-slate-400'>{route.description}</span>
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
