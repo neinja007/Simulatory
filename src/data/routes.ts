30a2cefd4bb5106470c3aadcdf87e4b04c85e6d4
neinja007 <anton.siligan@gmail.com>
2024-08-06 16:10:52 +0200

    rename routes.tsx to .ts

---
+++ src/data/routes.ts
@@ -0,0 +1,8 @@
+export const routes: { name: string; description: string; href: string }[] = [
+  {
+    name: "Conway's Game of Life",
+    description:
+      'Simulate and modify the legendary zero-player game of life, carefully devised by the British mathematician John Horton Conway in 1970.',
+    href: '/game-of-life'
+  }
+];
