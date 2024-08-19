3fb77c3032da4f00e320006d3190dd9769062540
neinja007 <anton.siligan@gmail.com>
2024-08-19 10:17:16 +0200

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Rubik } from 'next/font/google';
 import './globals.css';
 import { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/react';
 
 const rubik = Rubik({ subsets: ['latin'] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang='en' className={rubik.className}>
       <body className='bg-gradient-to-b from-white to-slate-300'>
         <div className='container min-h-screen pt-5 antialiased md:pt-8 lg:pt-12'>{children}</div>
+        <Analytics />
       </body>
     </html>
   );
